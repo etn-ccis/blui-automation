e055db4e476daa74ceccbcfa37497cd715bf7c72
joebochill <boyle.p.joseph@gmail.com>
2021-07-14 07:38:43 -0400

    Add check for first time publishing a new package

--- pxb-publish/scripts/publish.sh
+++ pxb-publish/scripts/publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 BRANCH=dev # default
+FIRST_TIME=false # is this the first time publishing the package?
 
 # Load the package name and current version from ./package.json
 PACKAGE=`node -p "require('./package.json').name"`
@@ -18,39 +19,51 @@ done
 NPM_LATEST_VERSION=`npm show ${PACKAGE} version`
 NPM_BETA_VERSION=`npm show ${PACKAGE}@beta version`
 NPM_ALPHA_VERSION=`npm show ${PACKAGE}@alpha version`
+if [ "$NPM_LATEST_VERSION" == "" ] 
+then FIRST_TIME=true; 
+fi;
 
 # Check if this is an alpha, beta, or latest package and run the appropriate publishing command
 if grep -q "alpha" <<< "$CURRENT_VERSION";
 then
-    if ! [ $CURRENT_VERSION == $NPM_ALPHA_VERSION ];
+    if ! [ "$CURRENT_VERSION" == "$NPM_ALPHA_VERSION" ];
     then
         echo "Publishing new alpha";
-        npm publish --tag alpha
+        if [ "$FIRST_TIME" == true ]
+        then npm publish --tag alpha --access public
+        else npm publish --tag alpha
+        fi
     else
         echo "Alpha version is already published."
     fi
 elif grep -q "beta" <<< "$CURRENT_VERSION";
 then
-    if ! [ $CURRENT_VERSION == $NPM_BETA_VERSION ];
+    if ! [ "$CURRENT_VERSION" == "$NPM_BETA_VERSION" ];
     then
         echo "Publishing new beta";
-        npm publish --tag beta
+        if [ "$FIRST_TIME" = true ]
+        then npm publish --tag beta --access public
+        else npm publish --tag beta
+        fi
     else
         echo "Beta version is already published."
     fi
 else
     # If this is not the master branch, do not do any 'latest' publications
-    if ! [ $BRANCH == "master" ];
+    if ! [ "$BRANCH" == "master" ];
     then
         echo "This is not the master branch - skipping publishing."
         exit 0;
     fi
 
     # If this is the master branch (or running locally without a -b flag), allow publishing a latest package
-    if ! [ $CURRENT_VERSION == $NPM_LATEST_VERSION ];
+    if ! [ "$CURRENT_VERSION" == "$NPM_LATEST_VERSION" ];
     then
         echo "Publishing new latest";
-        npm publish
+        if [ "$FIRST_TIME" = true ]
+        then npm publish --access public
+        else npm publish
+        fi
     else
         echo "Latest version is already published."
     fi
