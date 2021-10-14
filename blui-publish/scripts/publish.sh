47e5bdb764e249c0ef5f0260faf225265e5b4d95
emclaug2 <evan.w.mclaughlin@gmail.com>
2021-10-14 14:21:29 -0400

    Fix line enders

--- blui-publish/scripts/publish.sh
+++ blui-publish/scripts/publish.sh
@@ -4,6 +4,7 @@
 # All rights reserved.
 # This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
 
+
 BRANCH=dev # default
 FIRST_TIME=false # is this the first time publishing the package?
 
