a1131caba25aa79acd459016a90dbbb945604604
Evan <evan.w.mclaughlin@gmail.com>
2022-06-13 11:10:25 -0400

    Run prettier

--- blui-code-automation/index.js
+++ blui-code-automation/index.js
@@ -9,7 +9,9 @@ const calcAutomationPercentage = (unitTests, manualTests) => ((unitTests / (unit
 const requests = [
     axios.get('https://raw.githubusercontent.com/brightlayer-ui/angular-workflows/dev/login-workflow/VALIDATION.md'),
     axios.get('https://raw.githubusercontent.com/brightlayer-ui/react-workflows/dev/login-workflow/VALIDATION.md'),
-    axios.get('https://raw.githubusercontent.com/brightlayer-ui/react-native-workflows/dev/login-workflow/VALIDATION.md'),
+    axios.get(
+        'https://raw.githubusercontent.com/brightlayer-ui/react-native-workflows/dev/login-workflow/VALIDATION.md'
+    ),
     axios.get('https://raw.githubusercontent.com/brightlayer-ui/angular-component-library/dev/VALIDATION.md'),
     axios.get('https://raw.githubusercontent.com/brightlayer-ui/react-component-library/dev/VALIDATION.md'),
     axios.get('https://raw.githubusercontent.com/brightlayer-ui/brightlayer-ui-cli/dev/VALIDATION.md'),
